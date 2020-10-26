2967a88967abc3e790dc749a40c9069ee64e61d9
orangeOne <743316413@qq.com>
2020-10-26 10:29:47 +0800

    0000

--- src/newFile1.java
+++ src/newFile1.java
@@ -1,9 +1,11 @@
+import com.sun.org.apache.xpath.internal.operations.String;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class test {
+public class newFile1 {
     public static void main(String[] args) {
-        List<String> lists = new ArrayList<>();
+        List<java.lang.String> lists = new ArrayList<>();
         lists.add("122");
         System.out.println(lists.toString());
     }
